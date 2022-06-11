6eecb35aa5708ef48d909f125e084415ca579a58
KeziahOkyere41 <Nharnhayhaar208@gmail.com>
2022-06-11 13:47:57 +0000

    completed assignment

---
+++ index.js
@@ -0,0 +1,9 @@
+const divide=() =>
+2000/100;
+divide(20)
+
+const square=(parameter1) => parameter1 * parameter1;
+square(4)
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+add(2000,100)
